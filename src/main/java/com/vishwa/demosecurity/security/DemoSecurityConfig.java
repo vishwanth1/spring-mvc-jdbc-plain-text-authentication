15fb0868389c1bfd2458202750ddbc4129b339eb
Vishwanth Geddam <vgeddam1@leomail.tamuc.edu>
2023-06-28 17:59:33 -0500

    spring mvc plain text jdbc authentication

---
+++ src/main/java/com/vishwa/demosecurity/security/DemoSecurityConfig.java
@@ -0,0 +1,40 @@
+package com.vishwa.demosecurity.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.jaas.memory.InMemoryConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DemoSecurityConfig {
+
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(
+                configure -> configure
+                        .requestMatchers("/").hasRole("EMPLOYEE")
+                        .requestMatchers("/leaders/**").hasRole("MANAGER")
+                        .requestMatchers("/systems/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+        ).formLogin(form -> form.loginPage("/showMyLoginPage").
+                loginProcessingUrl("/authenticateTheUser").permitAll())
+                        .logout(logout -> logout.permitAll())
+                .exceptionHandling(configure -> configure.accessDeniedPage("/access-denied"));
+        
+        return httpSecurity.build();
+    }
+}
